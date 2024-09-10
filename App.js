f44f00044ee32dd09274378dc61f8f3930a5a166
FernandaMSC <fernanda.magalhaes.sc@gmail.com>
2024-09-09 22:11:25 -0300

    Bibliotecas de navegação
    Instaladas as bibliotecas necessárias para navegação

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+
 
  export default function App() {
   return (
-    <View style={styles.container}>
+    <NavigationContainer>        
+      <View style={styles.container}>
       <Text>Força! Falta pouco!</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
